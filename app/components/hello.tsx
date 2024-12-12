9d8d5f052616e0d74cee5163a5074660453f1841
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2024-12-12 13:39:21 +0530

    intro

---
+++ app/components/hello.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+function Hello() {
+   console.log("Hello Boss?");
+   console.log("This is a client-side code");
+   return (
+      <>
+         <h1 className="text-3xl">Client Side</h1>
+      </>
+   );
+}
+
+
+export default Hello;
