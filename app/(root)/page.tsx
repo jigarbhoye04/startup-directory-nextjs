1ccdcb0b5d6963ff33e49e7a3766132bb0280fb4
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2024-12-12 14:50:06 +0530

    Implement authentication with NextAuth and GitHub provider; add Navbar component for user session management

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+   console.log("What is this --server or client?");
+   return (
+      <>
+         <h1 className="text-2xl">HOME</h1>
+      </>
+   );
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { auth, signOut, signIn } from "@/auth";
+import { redirect } from "next/dist/server/api-utils";
+
+const Navbar = async () => {
+   const session = await auth();
+
+   return (
+      <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+         <nav className="flex justify-between items-center">
+            <Link href="https://www.google.com/">
+               <Image src="/logo.png" alt="logo" width={50} height={50} />
+            </Link>
+
+            <div className="flex items-center space-x-5 text-black">
+               {session && session?.user ? (
+                  <>
+                     <Link href="/startup/create">
+                        <span>Create</span>
+                     </Link>
+                     <form action={async () => {
+                        "use server";
+                        await signOut({ redirectTo: "/" });
+                        //should be like : await signOut(options?: { redirectTo: "/" });
+                        //but idk why its like this (still works so.. :)  )
+                     }}>
+                        <button type="submit">
+                            <span>Logout</span>
+                        </button>
+                     </form>
+
+                     {/* Dynamically links to user session */}
+                     <Link href={`/user/${session?.id}`}>
+                        <span>{session?.user?.name}</span>
+                     </Link>
+                  </>
+               ) : (
+                  <form
+                     action={async () => {
+                        "use server";
+                        await signIn("github");
+                     }}
+                  >
+                     <button type="submit">
+                        <span>Login</span>
+                     </button>
+                  </form>
+               )}
+            </div>
+         </nav>
+      </div>
+   );
+};
+
+export default Navbar;
