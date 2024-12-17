128b5d31d5b095adb01eeb8b01c0007614f0e659
jigx.04 <84662430+jigarbhoye04@users.noreply.github.com>
2024-12-17 15:51:53 +0530

    Fix query typo and enhance startup fetching logic with search parameters. Fixed Search Feature.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 // import { client } from "@/sanity/lib/client";
 import SearchForm from "../../components/SearchForm";
 import StartupCard, { StartupTypeCard } from "../../components/StartupCard";
-import { STARTUPS_QUERIY } from "@/sanity/lib/queries";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
@@ -10,8 +10,9 @@ export default async function Home({
    searchParams: Promise<{ query?: string }>;
 }) {
    const query = (await searchParams).query;
-   // const posts = await client.fetch(STARTUPS_QUERIY);
-   const { data: posts } = await sanityFetch({query: STARTUPS_QUERIY});
+   const params = { search: query || null };
+   // const posts = await client.fetch(STARTUPS_QUERY);
+   const { data: posts } = await sanityFetch({query: STARTUPS_QUERY,params});
    //this is a promise, so we need to await it to get the data immediately
    //revalidates data whenever the data changes
 
